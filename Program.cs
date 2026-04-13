b05524ca92b9a13760d6822c17cd0ae191d45809
neshi2170 <ST10480647@rcconnect.edu.za>
2026-04-13 20:47:50 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        UIHelper.DisplayLogo();
+
+        Console.Write("\nEnter your name: ");
+        string name = Console.ReadLine();
+
+        Chatbot bot = new Chatbot(name);
+        bot.Start();
+    }
+}
+
